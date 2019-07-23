6263574a28a1b824ebee9554bc7bf1f7de1da9fb
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-23 20:41:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,27 +18,12 @@ function whileLoop(n) {
   }
 
 function doWhileLoop(integer) {
-var i = 0;
-function incrementVariable() {
-
-i = i + 1;
-return i;
-}
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
 do {
-console.log("I run once regardless.");
-} while(incrementVariable() < integer);
-}
-
-
-
-function doWhileLoop(integer) {
-var i = 0;
-function incrementVariable() {
-
-i = i + 1;
-return i;
-}
-do {
-console.log("I run once regardless.");
+  console.log("I run once regardless.");
 } while(incrementVariable() < integer);
 }
